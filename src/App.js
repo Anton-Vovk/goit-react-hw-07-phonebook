958ecad84343c20406a6e5eb379e9739e3ec736e
Anton-Vovk <mr.vovk.anton@gmail.com>
2021-05-26 00:17:56 +0300

    update

--- src/App.js
+++ src/App.js
@@ -1,17 +1,19 @@
 import React from 'react';
-import styles from './App.module.css';
 import ContactForm from './components/ContactForm';
 import Filter from './components/Filter';
 import ContactList from './components/ContactList';
+import Container from './components/Container';
 
 const App = () => (
-  <div className={styles.container}>
-    <h1>Phonebook</h1>
-    <ContactForm />
-    <h2>Contacts</h2>
-    <Filter />
-    <ContactList />
-  </div>
+  <>
+    <Container>
+      <h1>Phonebook</h1>
+      <ContactForm />
+      <h2>Contacts</h2>
+      <Filter />
+      <ContactList />
+    </Container>
+  </>
 );
 
 export default App;
